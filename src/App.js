a7a3c7204d956e9c61421325b6235c608f283d47
PDMAU <PDMAU@users.noreply.github.com>
2023-05-10 00:54:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,76 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import { Container, Form, FormControl, ListGroup } from "react-bootstrap";
 import './App.css';
 
-function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const fruits = [
+  { name: "Banana" },
+  { name: "Apple" },
+  { name: "Orange" },
+  { name: "Strawberry" },
+  { name: "Grape" },
+  { name: "Kiwi" },
+  { name: "Mango" },
+  { name: "Pineapple" },
+  { name: "Watermelon" },
+];
 
-  const handleEmailChange = event => setEmail(event.target.value);
-  const handlePasswordChange = event => setPassword(event.target.value);
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fruitsPerPage] = useState(3);
 
-  const handleLogin = () => {
-    // Authenticate with email and password using Open API
-    console.log(`Logging in with email ${email} and password ${password}`);
-  };
+  // Filtro
+  const filteredFruits = fruits.filter((fruit) =>
+    fruit.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  const handleGoogleLogin = () => {
-    // Authenticate with Google using Open API
-    console.log('Logging in with Google');
-  };
+  // Paginação
+  const indexOfLastFruit = currentPage * fruitsPerPage;
+  const indexOfFirstFruit = indexOfLastFruit - fruitsPerPage;
+  const currentFruits = filteredFruits.slice(indexOfFirstFruit, indexOfLastFruit);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <button onClick={handleGoogleLogin}>Login with Google</button>
-    </div>
+    <Container className="mt-4">
+      <h1>Fruit List</h1>
+
+      <Form className="mb-3">
+        <FormControl
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </Form>
+
+      <ListGroup>
+        {currentFruits.map((fruit, index) => (
+          <ListGroup.Item key={index}>{fruit.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+
+      <nav>
+        <ul className="pagination">
+          {[...Array(Math.ceil(filteredFruits.length / fruitsPerPage)).keys()].map(
+            (pageNumber) => (
+              <li
+                key={pageNumber}
+                className={`page-item ${
+                  pageNumber + 1 === currentPage ? "active" : ""
+                }`}
+              >
+                <button className="page-link" onClick={() => paginate(pageNumber + 1)}>
+                  {pageNumber + 1}
+                </button>
+              </li>
+            )
+          )}
+        </ul>
+      </nav>
+    </Container>
   );
-}
+};
 
 export default App;
+
