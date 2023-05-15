e016bfe5b1729011098584f11df54c6535a066eb
maurilio.goncalves <maurilio.goncalves@sensedia.com>
2023-05-15 08:57:29 -0300

    Adicionado arquivos da pagina principal de listagem

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 const disciplines = [
   {
-    name: "Projetos ",
+    name: "Projetos",
     code: "MC 855",
     professor: "Licio",
     capacity: 50,
@@ -27,17 +27,117 @@ const disciplines = [
     professor: "Chat GPT",
     capacity: 60,
   },
+  // Mais 20 itens
+  {
+    name: "Engenharia de Software",
+    code: "MC 958",
+    professor: "Maria Silva",
+    capacity: 80,
+  },
+  {
+    name: "Redes de Computadores",
+    code: "MC 874",
+    professor: "Carlos Oliveira",
+    capacity: 70,
+  },
+  {
+    name: "Sistemas Operacionais",
+    code: "MC 816",
+    professor: "Lucas Santos",
+    capacity: 60,
+  },
+  {
+    name: "Banco de Dados",
+    code: "MC 833",
+    professor: "Juliana Ferreira",
+    capacity: 50,
+  },
+  {
+    name: "Inteligência Artificial",
+    code: "MC 999",
+    professor: "Rafael Garcia",
+    capacity: 40,
+  },
+  {
+    name: "Lógica para Computação",
+    code: "MC 803",
+    professor: "Patricia Costa",
+    capacity: 80,
+  },
+  {
+    name: "Estrutura de Dados",
+    code: "MC 722",
+    professor: "Fernando Oliveira",
+    capacity: 60,
+  },
+  {
+    name: "Programação Orientada a Objetos",
+    code: "MC 833",
+    professor: "Camila Souza",
+    capacity: 50,
+  },
+  {
+    name: "Análise de Algoritmos",
+    code: "MC 711",
+    professor: "Daniel Santos",
+    capacity: 40,
+  },
+  {
+    name: "Arquitetura de Computadores",
+    code: "MC 843",
+    professor: "Ricardo Mendes",
+    capacity: 80,
+  },
+  {
+    name: "Sistemas Distribuídos",
+    code: "MC 866",
+    professor: "Patricia Silva",
+    capacity: 70,
+  },
+  {
+    name: "Segurança da Informação",
+    code: "MC 911",
+    professor: "Gabriel Oliveira",
+    capacity: 60,
+  },
+  {
+    name: "Engenharia de Requisitos",
+    code: "MC 877",
+    professor: "Mariana Costa",
+    capacity: 50,
+  },
+  {
+    name: "Gestão de Projetos",
+    code: "MC 922",
+    professor: "Rafaela Santos",
+    capacity: 40,
+  },
+  {
+    name: "Compiladores",
+    code: "MC 888",
+    professor: "Henrique Ferreira",
+    capacity: 80,
+  },
+  {
+    name: "Computação Gráfica",
+    code: "MC 855",
+    professor: "Laura Oliveira",
+    capacity: 70,
+  },
 ];
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [disciplinesPerPage] = useState(3);
+  const [disciplinesPerPage] = useState(5);
 
   // Filtro
   const filteredDisciplines = disciplines.filter((discipline) =>
-    discipline.name.toLowerCase().includes(searchTerm.toLowerCase())
+    discipline.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    discipline.code.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
 
   // Paginação
   const indexOfLastDiscipline = currentPage * disciplinesPerPage;
@@ -51,18 +151,19 @@ const App = () => {
 
   return (
     <Container className="mt-4">
-      <h1>Discipline List</h1>
-
+          <div className="header">
+            <h1 className="text-center">Disciplinas oferecidades</h1>
+          </div>
       <Form className="mb-3">
         <FormControl
           type="text"
-          placeholder="Search by name"
+          placeholder="Filtrar disciplina"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </Form>
 
-      <Table striped bordered hover>
+      <Table striped bordered hover >
         <thead>
           <tr>
             <th>Name</th>
